015d77c3e78b1ecccee231fe28fa3d83b9e04a23
Freyasnow001 <freyasnow001@gmail.com>
2025-03-28 16:53:23 -0300

    hello

--- phytoplankton_plot-main/make_phytoplankton_plot.R
+++ phytoplankton_plot-main/make_phytoplankton_plot.R
@@ -6,7 +6,7 @@ data <- read.csv("phytoplankton_data.csv", header =TRUE)
 
 # Make plot
 ggplot(data, aes(x=month, y=phytoplankton)) + 
-  geom_line(colour = "red", size=2) + 
+  geom_line(colour = "red", linewidth=2) + 
   labs(title="Phytoplankton concentration in Nova Scotia",
        subtitle="units: milimoles/m3",
        caption="Source: NOAA, ERDDAP") +
